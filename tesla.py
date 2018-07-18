65d52a7949f6a1603425ca021654b8ee728de92f
Conor Manley <conor.manley94@gmail.com>
2018-07-18 09:39:36 -0400

    Updated sim to use preemptive resource to handle thermoformer cycles

--- tesla.py
+++ tesla.py
@@ -6,10 +6,8 @@ import time
 
 
 class G:
-	move_map = ([["Pos", "100", "110", "120"], 
-								["100", 0, 8, 5], 
-								["110", 8, 0, 14], 
-								["120", 5, 14, 0]])
+	#Environment constants
+	SIMULATION_TIME = 7200
 	
 	ROUTER_CAPACITY = 1
 	ROUTER_RUNTIME = 90
@@ -28,69 +26,67 @@ class G:
 	LOAD_STATION_UNLOAD_TIME = 7
 	LOAD_STATION_CAPACITY = 2
 	
-	RAW_SHEET_STOCK_SIZE = 1
+	RAW_SHEET_STOCK_SIZE = 2
 	FORMED_SHEET_STOCK_SIZE = 2
 	ROUTED_PART_STOCK_SIZE = 1000
 	
-	
-	
-	
-
 
 class Operator(simpy.Resource):
 	def __init__(self, env):
 		super(Operator, self).__init__(env, capacity=1)
 		self.env = env
 
+
 class Thermoformer(object):
-	def __init__(self, name, env, load_station):
+	def __init__(self, name, env, station, load_station):
+		self.station = station
 		self.load_station = load_station
 		self.loaded_stock = load_station.finished_stock
 		self.oven_stock = simpy.Container(env, 1)
 		self.mold_stock = simpy.Container(env, 1)
 		self.name = name
 		self.env = env
-		self.parts = 0
+		self.cycles = 0
+		self.failures = 0
 		self.process = env.process(self.run(self.env))
 		
 	def run(self, env):
 		while True:		
 			yield env.timeout(G.THERMOFORMER_RUNTIME)
-			if self.load_station.process:
-				self.load_station.process.interrupt()
-			
-			print("Ran a cycle at {0}".format(env.now))
-			if self.mold_stock.level > 0:
-				yield self.mold_stock.get(1)
-				self.load_station.status = 'COMPLETE'
-				print("{0} created a formed sheet at {1}".format(self.name, env.now))
-			else:
-				self.load_station.status = 'EMPTY'
-			
-			if self.oven_stock.level > 0:
-				yield self.oven_stock.get(1)
-				yield self.mold_stock.put(1)
-				print('Sheet has been put in the mold at {0}'.format(env.now))
-			
-			if self.load_station.capacity.level == G.LOAD_STATION_CAPACITY:
-				print('Putting a sheet in the oven at {0}'.format(env.now))
-				yield self.oven_stock.put(1)
-				print('Sheet has been put in the oven at {0}'.format(env.now))
-			else:
-				print('FAILED TO LOAD THERMOFORMER IN TIME')
-			
-			if self.load_station.capacity.level > 0:
-				yield self.load_station.capacity.get(self.load_station.capacity.level) #Consume all loaded raw sheet stock
-			
-			env.process(self.load_station.run(self.load_station.operator, self.env))
-			
-			
-
-			
+			with station.request(priority=0) as st:
+				yield st
+				self.cycles += 1
+				print("Ran a cycle at {0}".format(env.now))
+				if self.mold_stock.level > 0:
+					yield self.mold_stock.get(1)
+					self.load_station.status = 'COMPLETE'
+					print("{0} created a formed sheet at {1}".format(self.name, env.now))
+					yield self.load_station.finished_stock.put(G.THERMOFORMER_YIELD)
+				else:
+					self.load_station.status = 'EMPTY'
+				
+				if self.oven_stock.level > 0:
+					yield self.oven_stock.get(1)
+					yield self.mold_stock.put(1)
+					print('Sheet has been put in the mold at {0}'.format(env.now))
+				
+				if self.load_station.capacity.level == G.LOAD_STATION_CAPACITY:
+					print('Putting a sheet in the oven at {0}'.format(env.now))
+					yield self.oven_stock.put(1)
+					print('Sheet has been put in the oven at {0}'.format(env.now))
+				else:
+					print('FAILED TO LOAD THERMOFORMER IN TIME')
+					self.failures += 1
+				
+				if self.load_station.capacity.level > 0:
+					yield self.load_station.capacity.get(self.load_station.capacity.level) #Consume all loaded raw sheet stock
+				
+				env.process(self.load_station.run(self.load_station.operator, self.env))
 
 
 class Load_Station(object):
-	def __init__(self, name, env, operator, raw_stock, finished_stock):
+	def __init__(self, name, env, operator, station, raw_stock, finished_stock):
+		self.station = station
 		self.raw_stock = raw_stock
 		self.finished_stock = finished_stock
 		self.name = name
@@ -102,51 +98,46 @@ class Load_Station(object):
 		self.process = env.process(self.run(self.operator, self.env))
 		
 	def run(self, operator, env):
-		while True:
-			try:
-				if self.status == 'EMPTY':
-					yield self.raw_stock.get(1)
-					load_proc = env.process(self.load_sheet(self.operator, self.env))
-					yield load_proc
-				
-				if self.status == 'COMPLETE':
-					yield self.finished_stock.put(G.THERMOFORMER_YIELD)
-					unload_proc = env.process(self.unload_sheet(self.operator, self.env))
-					yield unload_proc
-				
-				if self.status == 'READY':
-					yield env.timeout(10)
-			except simpy.Interrupt:
-				try:
-					load_proc.interrupt()
-				except:
-					pass
-				try:
-					unload_proc.interrupt()
-				except:
-					pass
-	
+		while self.status != 'READY':
+			if self.status == 'EMPTY':
+				yield self.raw_stock.get(1)
+				load_proc = env.process(self.load_sheet(self.operator, self.env))
+				yield load_proc
+			
+			if self.status == 'COMPLETE':
+				unload_proc = env.process(self.unload_sheet(self.operator, self.env))
+				yield unload_proc
+			
 	def unload_sheet(self, operator, env):
-		try:
-			with operator.request() as opr:
-				yield opr
-				yield env.timeout(G.LOAD_STATION_UNLOAD_TIME)
-				print("{0} unloaded a formed sheet at {1}".format(self.name, env.now))
-				self.status = 'EMPTY'
-		except simpy.Interrupt:
-			pass
-	
+		with self.station.request(priority=100) as st:
+			yield st
+			try:
+				with operator.request() as opr:
+					yield opr
+					yield env.timeout(G.LOAD_STATION_UNLOAD_TIME)
+					print("{0} unloaded a formed sheet at {1}".format(self.name, env.now))
+					self.status = 'EMPTY'
+			except simpy.Interrupt as interrupt:
+				by = interrupt.cause.by
+				usage = env.now - interrupt.cause.usage_since
+				print('unload_sheet on {0} got preempted by {1} after {2}'.format(self.name, by, usage))
+
 	def load_sheet(self, operator, env):
-		try:
-			with operator.request() as opr:
-				yield opr
-				yield env.timeout(G.LOAD_STATION_LOAD_TIME)
-				yield self.capacity.put(1)
-				print("{0} loaded a sheet at {1}".format(self.name, env.now))
-				if self.capacity.level == G.LOAD_STATION_CAPACITY:
-					self.status = 'READY'
-		except simpy.Interrupt:
-			pass
+		with self.station.request(priority=100) as st:
+			yield st
+			try:
+				with operator.request() as opr:
+					yield opr
+					yield env.timeout(G.LOAD_STATION_LOAD_TIME)
+					yield self.capacity.put(1)
+					print("{0} loaded a sheet at {1}".format(self.name, env.now))
+					if self.capacity.level == G.LOAD_STATION_CAPACITY:
+						self.status = 'READY'
+			except simpy.Interrupt as interrupt:
+				by = interrupt.cause.by
+				usage = env.now - interrupt.cause.usage_since
+				print('load_sheet on {0} got preempted by {1} after {2}'.format(self.name, by, usage))
+
 
 class Router(object):
 	def __init__(self, name, env, operator, raw_stock, finished_stock):
@@ -170,12 +161,10 @@ class Router(object):
 				yield env.process(self.unload_part(self.operator, self.env))
 			
 			if self.status == 'READY':
-				with operator.request() as opr:
-					yield opr
-					yield env.timeout(G.ROUTER_RUNTIME)
-					self.parts += G.ROUTER_YIELD
-					print("{0} completed a part at {1}".format(self.name, env.now))
-					self.status = 'COMPLETE'
+				yield env.timeout(G.ROUTER_RUNTIME)
+				self.parts += G.ROUTER_YIELD
+				print("{0} completed a part at {1}".format(self.name, env.now))
+				self.status = 'COMPLETE'
 
 	def unload_part(self, operator, env):
 		with operator.request() as opr:
@@ -190,6 +179,7 @@ class Router(object):
 			yield env.timeout(G.ROUTER_LOAD_TIME)
 			print("{0} loaded a part at {1}".format(self.name, env.now))
 			self.status = 'READY'
+
 
 class Sheeter(object):
 	def __init__(self, name, env, operator, finished_stock):
@@ -223,8 +213,8 @@ class Sheeter(object):
 env = simpy.Environment()
 
 #Operators
-main_op = Operator(env)
-sup_op = Operator(env)
+main_ops = Operator(env)
+sup_ops = Operator(env)
 
 #Containers
 formed_sheet_stock = simpy.Container(env, G.FORMED_SHEET_STOCK_SIZE, init=0)
@@ -232,18 +222,20 @@ routed_part_stock = simpy.Container(env, G.ROUTED_PART_STOCK_SIZE, init=0)
 raw_sheet_stock = simpy.Container(env, G.RAW_SHEET_STOCK_SIZE, init=0)
 
 #Thermoformers
-load_station_one = Load_Station('Load Station 1', env, main_op, raw_sheet_stock, formed_sheet_stock)
-thermoformer_one = Thermoformer('Thermoformer 1', env, load_station_one)
+station = simpy.PreemptiveResource(env, capacity=1)
+load_station_one = Load_Station('Load Station 1', env, main_ops, station, raw_sheet_stock, formed_sheet_stock)
+thermoformer_one = Thermoformer('Thermoformer 1', env, station, load_station_one)
 	
 #Automatic Sheeters
-sheeter_one = Sheeter('Sheeter 1', env, main_op, raw_sheet_stock)
+sheeter_one = Sheeter('Sheeter 1', env, main_ops, raw_sheet_stock)
 
 #Robotic Routers
-router_one = Router('Router 1', env, main_op, formed_sheet_stock, routed_part_stock)
-router_two = Router('Router 2', env, sup_op, formed_sheet_stock, routed_part_stock)
+router_one = Router('Router 1', env, main_ops, formed_sheet_stock, routed_part_stock)
+router_two = Router('Router 2', env, sup_ops, formed_sheet_stock, routed_part_stock)
 
-env.run(until=1000)
+env.run(until=G.SIMULATION_TIME)
+print('{0} produced {1} sheets'.format(sheeter_one.name, sheeter_one.sheets))
 print('{0} produced {1} parts and {2} produced {3} parts.'.format(router_one.name, router_one. parts, router_two.name, router_two.parts))
-
+print('{0} missed {1} of {2} total cycles'.format(thermoformer_one.name, thermoformer_one.failures, thermoformer_one.cycles))
 
 
